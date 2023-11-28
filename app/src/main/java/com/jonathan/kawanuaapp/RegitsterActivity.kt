95bc16b28d8f0ed8b5b7e7154fb7c7bdee5554e1
JonathanK <jkamag41@gmail.com>
2023-11-28 21:26:06 +0800

    Adding Register Activity

---
+++ app/src/main/java/com/jonathan/kawanuaapp/RegitsterActivity.kt
@@ -0,0 +1,11 @@
+package com.jonathan.kawanuaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegitsterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regitster)
+    }
+}
