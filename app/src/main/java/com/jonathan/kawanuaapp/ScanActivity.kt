41f7112000aa255dad06f4cad11b2d6facffcd71
JonathanK <jkamag41@gmail.com>
2023-11-29 18:05:18 +0800

    Change the activity name to ScanActivity

--- app/src/main/java/com/jonathan/kawanuaapp/ScanActivity.kt
+++ app/src/main/java/com/jonathan/kawanuaapp/ScanActivity.kt
@@ -3,7 +3,7 @@ package com.jonathan.kawanuaapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Scan : AppCompatActivity() {
+class ScanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
