25746a81498f1a8a036badd8896f297f9099565f
JonathanK <jkamag41@gmail.com>
2023-11-29 12:04:27 +0800

    Completing the Register Activity, Add Fonts, and setting the color resource

--- app/src/main/java/com/jonathan/kawanuaapp/RegisterActivity.kt
+++ app/src/main/java/com/jonathan/kawanuaapp/RegisterActivity.kt
@@ -3,9 +3,9 @@ package com.jonathan.kawanuaapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class RegitsterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_regitster)
+        setContentView(R.layout.activity_register)
     }
 }
