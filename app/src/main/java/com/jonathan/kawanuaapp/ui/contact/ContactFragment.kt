5e97a2f59c369eabe80c284db237851b6a0cf3cd
JonathanK <jkamag41@gmail.com>
2023-12-10 21:57:22 +0800

    .

---
+++ app/src/main/java/com/jonathan/kawanuaapp/ui/contact/ContactFragment.kt
@@ -0,0 +1,40 @@
+package com.jonathan.kawanuaapp.ui.contact
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.jonathan.kawanuaapp.databinding.FragmentContactBinding
+import com.jonathan.kawanuaapp.databinding.FragmentScanBinding
+
+class ContactFragment : Fragment() {
+
+    private var _binding: FragmentScanBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val notificationsViewModel =
+            ViewModelProvider(this).get(ContactViewModel::class.java)
+
+        _binding = FragmentScanBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
