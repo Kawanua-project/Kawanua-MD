7a44eae5e5b8ea8a7e07b04d05eb63b2e8c64cdb
Jonathan K <jkamagi41@gmail.com>
2023-12-14 05:50:53 +0800

    Adding the view model of contact fragment

--- app/src/main/java/com/jonathan/kawanuaapp/ui/contact/ContactViewModel.kt
+++ app/src/main/java/com/jonathan/kawanuaapp/ui/contact/ContactViewModel.kt
@@ -3,11 +3,15 @@ package com.jonathan.kawanuaapp.ui.contact
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.jonathan.kawanuaapp.model.Konservasi
 
 class ContactViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+    private val _listKonservasi = MutableLiveData<ArrayList<Konservasi>>()
+    val listKonservasi: LiveData<ArrayList<Konservasi>> = _listKonservasi
+
+    fun search() {
+
     }
-    val text: LiveData<String> = _text
+
 }
